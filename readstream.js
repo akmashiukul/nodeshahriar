3621184b83649ebf60b92dc68de64e30a713900c
akmashiukul <akmashikul@gmail.com>
2021-07-31 16:06:10 +0600

    add

--- readstream.js
+++ readstream.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const data = fs.createReadStream('1.txt', 'utf8')
+const write = fs.createWriteStream('output.txt')
 data.on('data', (chunk) => {
-    console.log(chunk.toString())
+    write.write(chunk)
+
 })
